6ae9a6c7be3fa23656d088cfd2a6de719ace4577
Newtan Quinlan <newtanquinlan@toans-mbp.attlocal.net>
2018-04-29 12:57:52 -0700

    Start a new Claim button added

--- screens/Home/HomeScreen.js
+++ screens/Home/HomeScreen.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
-import { Text, View, SafeAreaView, StyleSheet, Dimensions } from 'react-native'
+import { Text, View, SafeAreaView, StyleSheet, Dimensions, TouchableOpacity } from 'react-native'
 import TextLetterSpacing from 'react-native-letter-spacing';
+import Icon from 'react-native-vector-icons/Octicons';
 
 import Search from '../../components/Search/Search'
 import CarouselList from '../../components/Carousel/CarouselList'
 import Filter from '../../components/Filter/Filter'
+import Fonts from '../../utils/fonts'
 
 import {itemWidth} from '../../utils/carousel'
 
@@ -25,7 +27,15 @@ export default class HomeScreen extends Component {
             <Filter />
           </View>
         </View>
-        <CarouselList></CarouselList>
+        <View style = {styles.cards}>
+          <CarouselList></CarouselList>
+        </View>
+        <View style={styles.btnContainer}>
+          <TouchableOpacity style={styles.button}>
+              <Icon size={30} name="plus" color="#fff"/>
+            <Text style={styles.buttonText}>Start a new Claim</Text>
+          </TouchableOpacity>
+        </View>
       </SafeAreaView>
     )
   }
@@ -41,6 +51,7 @@ const styles = StyleSheet.create({
     padding: 30,
     paddingLeft: 0,
     paddingRight: 0,
+    paddingBottom: 20,
     width: itemWidth - 20,
     alignSelf: 'center'
   },
@@ -50,7 +61,7 @@ const styles = StyleSheet.create({
     display: 'flex',
     flexDirection: 'row',
     alignItems: 'center',
-    marginBottom: 5
+    marginBottom: 10,
   },
   textContainer: {
     flex: 1
@@ -62,5 +73,33 @@ const styles = StyleSheet.create({
   },
   filter: {
     
+  },
+  cards: {
+    flex: 5
+  },
+  btnContainer: {
+    flex: 1,
+    width: itemWidth - 20,
+    alignSelf: 'center',
+
+    display:'flex',
+    alignItems: 'center'
+  },
+  button: {
+    backgroundColor: '#d94046',
+    paddingTop:20,
+    paddingBottom: 20,
+    paddingLeft: 30,
+    paddingRight: 30,
+    borderRadius: 30,
+    display:'flex',
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 20,
+    fontFamily: Fonts.MontserratSemiBold,
+    marginLeft: 10
   }
 })
